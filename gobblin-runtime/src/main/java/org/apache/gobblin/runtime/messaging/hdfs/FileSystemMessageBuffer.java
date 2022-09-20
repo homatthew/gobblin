1edb6523aad1e71694b11965c31335ae20dbf2a1
hanghangliu <nate.hanghang.liu@gmail.com>
2022-09-19 21:51:43 -0700

    implement FileSystemMessageBuffer
    address comments
    
    update publish method

--- gobblin-runtime/src/main/java/org/apache/gobblin/runtime/messaging/hdfs/FileSystemMessageBuffer.java
+++ gobblin-runtime/src/main/java/org/apache/gobblin/runtime/messaging/hdfs/FileSystemMessageBuffer.java
@@ -16,47 +16,74 @@
  */
 package org.apache.gobblin.runtime.messaging.hdfs;
 
+import com.google.common.io.ByteStreams;
 import com.typesafe.config.Config;
+
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.gobblin.runtime.messaging.MessageBuffer;
 import org.apache.gobblin.runtime.messaging.data.DynamicWorkUnitMessage;
 import org.apache.gobblin.runtime.messaging.data.DynamicWorkUnitSerde;
+import org.apache.gobblin.util.ConfigUtils;
+import org.apache.gobblin.util.PathUtils;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+
+import static org.apache.gobblin.runtime.messaging.DynamicWorkUnitConfigKeys.*;
 
 
 /**
  * Implements {@link FileSystem} based message buffer for sending and receiving {@link DynamicWorkUnitMessage}.
  */
+@Slf4j
 public class FileSystemMessageBuffer implements MessageBuffer<DynamicWorkUnitMessage> {
+
+  // Dynamic Workunit message specific file suffix
+  private static final String messageFileSuffix = ".wumessage";
+
   private boolean isPollingTaskScheduled = false;
 
   @Getter
   private final String channelName;
   private final FileSystem fs;
-  private final Path dir;
+  @Getter
+  private final Path path;
   private final long pollingRateMillis;
   private final List<Consumer<List<DynamicWorkUnitMessage>>> subscribers = new ArrayList<>();
 
-  public FileSystemMessageBuffer(FileSystem fs, String channelName, long pollingRateMillis, Path parentWorkingDir) {
+  private FileSystemMessageBuffer(FileSystem fs, String channelName, long pollingRateMillis, Path parentWorkingDir) {
     this.fs = fs;
     this.channelName = channelName;
     this.pollingRateMillis = pollingRateMillis;
-    this.dir = Path.mergePaths(parentWorkingDir, new Path(channelName));
+    this.path = parentWorkingDir;
   }
 
   @Override
   public void publish(DynamicWorkUnitMessage item) throws IOException {
     byte[] serializedMsg = DynamicWorkUnitSerde.serialize(item);
-    persistMessage(serializedMsg);
+    try (FSDataOutputStream os = fs.create(this.getFilePathForMessage(item), true)) {
+      os.write(serializedMsg);
+      log.info("Persisted message into HDFS path {} for workunit {}.", path.toString(), item.getWorkUnitId());
+    } catch (IOException ioException) {
+      log.error("Failed to persist message into HDFS path {} for workunit {}.",
+          path.toString(), item.getWorkUnitId(), ioException);
+      throw ioException;
+    }
   }
 
   @Override
@@ -86,38 +113,68 @@ public class FileSystemMessageBuffer implements MessageBuffer<DynamicWorkUnitMes
   }
 
   private List<DynamicWorkUnitMessage> getNewMessages() {
-    // STUB: TODO GOBBLIN-1685 Read all files in dir, deserialize, then clean up byte files in dir
-    return null;
+    List<DynamicWorkUnitMessage> messageList = new LinkedList<>();
+    try {
+      FileStatus[] fileStatus = fs.listStatus(path);
+      for(FileStatus status : fileStatus){
+        Path singlePath = status.getPath();
+        if (fs.isFile(singlePath))
+        {
+          FSDataInputStream fis = fs.open(singlePath);
+          DynamicWorkUnitMessage message = DynamicWorkUnitSerde.deserialize(ByteStreams.toByteArray(fis));
+          messageList.add(message);
+          fs.delete(singlePath, true);
+          log.debug("Fetched message from HDFS path {}.", singlePath.toString());
+        }
+      }
+    } catch (IOException ioException) {
+      log.error("Failed to get message from HDFS path {}.", path.toString(), ioException);
+    }
+
+    return messageList;
   }
 
-  private void persistMessage(byte[] serializeMsg) throws IOException {
-    // STUB: TODO GOBBLIN-1685 write serialized message to FileSystem
+  private Path getFilePathForMessage(DynamicWorkUnitMessage message) {
+    String messageFileName = message.getWorkUnitId() + "_" + System.currentTimeMillis();
+    Path messagePath = PathUtils.mergePaths(path, new Path(messageFileName));
+    return PathUtils.addExtension(messagePath, messageFileSuffix);
   }
 
   public static class Factory implements MessageBuffer.Factory<DynamicWorkUnitMessage> {
     private static volatile ScheduledExecutorService executorSingleton;
-    private Config cfg;
+    private final Config cfg;
+    private Path basePath;
+    private FileSystem fileSystem;
+    private long pollingRate;
     public Factory(Config cfg) {
-      // STUB: TODO GOBBLIN-1685 Add any necessary setup
       this.cfg = cfg;
+      this.setup();
     }
 
     @Override
     public MessageBuffer<DynamicWorkUnitMessage> getBuffer(String channelName) {
-      // STUB: TODO GOBBLIN-1685 Construct buffer object based on Typesafe Config
-      return null;
+      Path workunitPath = new Path(basePath, channelName);
+      log.info("HDFS directory for dynamic workunit is: " + workunitPath);
+
+      return new FileSystemMessageBuffer(fileSystem, channelName, pollingRate, workunitPath);
     }
 
-    private FileSystem getFileSystem(Config cfg) {
-      // STUB: TODO GOBBLIN-1685 Get filesystem based on current state
-      return null;
+    private void setup() {
+      String fsPathString = this.cfg.getString(DYNAMIC_WORKUNIT_HDFS_PATH);
+      this.basePath = new Path(fsPathString);
+      try {
+        this.fileSystem = this.basePath.getFileSystem(new Configuration());
+      } catch (IOException e) {
+        throw new RuntimeException("Unable to detect workunit directory file system:", e);
+      }
+      pollingRate = ConfigUtils.getLong(this.cfg, DYNAMIC_WORKUNIT_HDFS_POLLING_RATE_MILLIS,
+          DYNAMIC_WORKUNIT_HDFS_DEFAULT_POLLING_RATE);
     }
 
     private synchronized static ScheduledExecutorService getExecutorInstance() {
       if (executorSingleton == null) {
         executorSingleton = Executors.newScheduledThreadPool(1);
       }
-
       return executorSingleton;
     }
   }
